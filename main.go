36d90d73d1bdcf8d3f23d60163554a89544b6682
Lars <lschwan43@gmail.com>
2025-11-12 18:22:18 +0100

    echo was useless, using net/http

--- main.go
+++ main.go
@@ -1,19 +1,32 @@
 package main
 
 import (
+	"fmt"
+	"io"
+	"log"
 	"net/http"
-
-	"github.com/labstack/echo/v4"
 )
 
-func FetchGithubCICDEnvironment() {
-	e := echo.New()
-	e.GET("", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Test")
-	})
-	e.Logger.Fatal(e.Start(":1323"))
+func FetchGithubCICDEnvironment() error {
+	const url = "https://api.github.com/repos/l3montree-dev/devguard-web/pulls/581"
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return fmt.Errorf("request failed: %w", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("read body failed: %w", err)
+	}
+
+	fmt.Printf("GET %s\nStatus: %s\n\n%s\n", url, resp.Status, body)
+	return nil
 }
 
 func main() {
-	FetchGithubCICDEnvironment()
+	if err := FetchGithubCICDEnvironment(); err != nil {
+		log.Fatal(err)
+	}
 }
