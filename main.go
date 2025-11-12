5cb445626fce53fcf2e12f992507e52d0c9064b2
Lars <lschwan43@gmail.com>
2025-11-12 17:59:51 +0100

    init commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func FetchGithubCICDEnvironment() {
+	e := echo.New()
+	e.GET("", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Test")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func main() {
+	FetchGithubCICDEnvironment()
+}
