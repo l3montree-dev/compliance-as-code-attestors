978198bac1d1cef45d7e004f97cda98c64670b99
Lars <lschwan43@gmail.com>
2025-11-14 13:52:41 +0100

    fixxed it not prinitng now iterating though array of repositories and giving every pr

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 )
 
@@ -11,34 +12,34 @@ type tempInput struct {
 	initRepoTitle  string   // entrypoint argument given by ci-cd pipeline
 }
 
-func FetchGithubCICDEnvironment(argument string) {
-	resp, err := http.Get(argument)
-	fmt.Println(resp)
-	if err != nil {
-		fmt.Println("Kaboom")
-	}
-}
-
 func AllRepositoryRequests(input tempInput) {
 
-	url := "https://api.github.com/repos/" + input.repositories[0] + "/issues?state=all"
-	resp, err := http.Get(url)
-	fmt.Println(resp)
-	if err != nil {
-		fmt.Println("Kaboom")
+	for index, _ := range input.repositories {
+		url := "https://api.github.com/repos/" + input.repositories[index] + "/issues?state=all"
+		resp, err := http.Get(url)
+
+		if err != nil {
+			fmt.Printf("whoops")
+		}
+
+		defer resp.Body.Close()
+		body, err := io.ReadAll(resp.Body)
+
+		fmt.Printf("%s", body)
+		if err != nil {
+			fmt.Println("Kaboom")
+		}
+
 	}
 
 }
 
 func main() {
 	exampleInput := tempInput{
-		repositories:   []string{"l3montree-dev/devguard-web", "l3montree-dev/devguard-web"},
+		repositories:   []string{"l3montree-dev/devguard-web", "l3montree-dev/devguard-documentation"},
 		initRepoNumber: 581,
 		initRepoTitle:  "1277 organization wide dependency search",
 	}
-	// FetchGithubCICDEnvironment("https://api.github.com/repos/l3montree-dev/devguard-web/pulls/581")
+
 	AllRepositoryRequests(exampleInput)
 }
-
-// curl -s https://api.github.com/repos/l3montree-dev/devguard-web/pulls/581
-// curl https://api.github.com/repos/l3montree-dev/devguard-documentation/issues?state=all
