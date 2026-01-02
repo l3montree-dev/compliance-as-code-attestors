073d81b16a7eb7c317f1a03dfae35fe8ac8521ad
Lars <lschwan43@gmail.com>
2026-01-02 15:45:10 +0100

    refactor and have input now differentiated

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ FROM golang:1.25 AS builder
 WORKDIR /production-process
 
 COPY . .
-RUN CGO_ENABLED=0 go build -o prAttest
+RUN CGO_ENABLED=0 go build -o prAttestation
 
 
 # create final image
@@ -12,7 +12,7 @@ RUN CGO_ENABLED=0 go build -o prAttest
 FROM ubuntu:latest
 RUN apt-get update && apt-get install -y ca-certificates && rm -rf /var/lib/apt/lists/*
 WORKDIR /
-COPY --from=builder /production-process/prAttest /usr/local/bin/prAttest
-RUN chmod +x /usr/local/bin/prAttest
+COPY --from=builder /production-process/prAttestation /usr/local/bin/prAttestation
+RUN chmod +x /usr/local/bin/prAttestation
 
-ENTRYPOINT ["/usr/local/bin/prAttest"]
+ENTRYPOINT ["/usr/local/bin/prAttestation"]
--- cmd/prAttest.go
+++ cmd/prAttest.go
@@ -11,9 +11,9 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// prAttestCmd represents the prAttest command
-var prAttestCmd = &cobra.Command{
-	Use:   "prAttest",
+// prAttestationCmd represents the prAttestation command
+var prAttestationCmd = &cobra.Command{
+	Use:   "prAttestation",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -23,8 +23,8 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 
-		initRepoTitle, err := cmd.Flags().GetString("initRepoTitle")
-		initRepoNumber, err := cmd.Flags().GetInt("initRepoNumber")
+		pull_request_title, err := cmd.Flags().GetString("pull_request_title")
+		pull_request_number, err := cmd.Flags().GetInt("pull_request_number")
 		rawRepos, err := cmd.Flags().GetStringArray("repos")
 
 		if err != nil {
@@ -43,23 +43,23 @@ to quickly create a Cobra application.`,
 			}
 		}
 
-		cross_pr_request.CrossPRrequest(repos, initRepoNumber, initRepoTitle)
+		cross_pr_request.CrossPRrequest(repos, pull_request_number, pull_request_title)
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(prAttestCmd)
+	rootCmd.AddCommand(prAttestationCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	prAttestCmd.Flags().Int("initRepoNumber", 3, "insert pullRequest Number here")
-	// prAttestCmd.Flags().StringArrayVar(["test"], "test", "test2")
-	prAttestCmd.Flags().StringArray("repos", []string{"l3montree-dev/devguard,l3montree-dev/devguard-documentation"}, "Comma-separated list of repositories or repeat --repos for each.")
-	prAttestCmd.Flags().String("initRepoTitle", "1277 organization wide dependency search", "Name of the Pull Request")
+	prAttestationCmd.Flags().Int("pull_request_number", 3, "insert pullRequest Number here")
+	// prAttestationCmd.Flags().StringArrayVar(["test"], "test", "test2")
+	prAttestationCmd.Flags().StringArray("repos", []string{"l3montree-dev/devguard,l3montree-dev/devguard-documentation"}, "Comma-separated list of repositories or repeat --repos for each.")
+	prAttestationCmd.Flags().String("pull_request_title", "1277 organization wide dependency search", "Name of the Pull Request")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// prAttestCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// prAttestationCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
